b912e1c13bc04a66232d39374e129be59820b887
caio lucio <caioluci1@hotmail.com>
2021-05-30 14:18:54 -0300

    Adicionando-app

---
+++ VirtualLibrary.Api/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualLibrary.Models;
+
+namespace virtualLibrary.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(string nome,string idade, string blabla)
+        {
+            return $"Meu nome: {nome}, /n idade: {idade}";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+    }
+}
